d3ebd47cfc2d9062872c4d8eb1c388bf2e68d82d
Duke <dukekhaos@gmail.com>
2016-08-11 16:06:03 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "globals": {
+    "page": false,
+    "EventDispatcher": true,
+    "accounting": true,
+    "I18n": true,
+    "UAParser": false,
+    "YouseChat": false,
+    "mixpanel": true,
+    "autosize": false,
+    "latinize": true
+  },
+  "env": {
+    "browser": true,
+    "amd": true,
+    "jquery": true
+  },
+  "rules": {
+    "quotes": [2, "single"],
+    "comma-dangle": [2, "never"],
+    "indent": [2, 2, { "VariableDeclarator": 2 }],
+    "camelcase": 2,
+    "no-multi-spaces": 0,
+    "no-shadow": 2,
+    "no-eq-null": 2,
+    "no-extra-parens": 2,
+    "no-lonely-if": 2,
+    "no-nested-ternary": 2,
+    "no-param-reassign": 2,
+    "no-self-compare": 2,
+    "no-throw-literal": 2,
+    "no-void": 2,
+    "no-undef": 2,
+    "no-extra-semi": 2,
+    "no-underscore-dangle": 0,
+    "eqeqeq": 2,
+    "comma-style": [2, "last"],
+    "semi": [2, "always"]
+  }
+}
