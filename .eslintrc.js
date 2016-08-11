0e2bc01d2b1b305534e1b1ee329bf50989417c46
Duke <dukekhaos@gmail.com>
2016-08-11 16:52:03 -0300

    organize .eslintrc config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   'globals': {
-    'page': false,
-    'EventDispatcher': true,
     'accounting': true,
-    'I18n': true,
-    'UAParser': false,
-    'YouseChat': false,
-    'mixpanel': true,
     'autosize': false,
-    'latinize': true
+    'EventDispatcher': true,
+    'I18n': true,
+    'latinize': true,
+    'mixpanel': true,
+    'module': true,
+    'page': false,
+    'UAParser': false,
+    'YouseChat': false
   },
   'env': {
     'browser': true,
@@ -16,25 +17,25 @@ module.exports = {
     'jquery': true
   },
   'rules': {
-    'quotes': [2, 'single'],
-    'comma-dangle': [2, 'never'],
-    'indent': [2, 2, { 'VariableDeclarator': 2 }],
     'camelcase': 2,
-    'no-multi-spaces': 0,
-    'no-shadow': 2,
+    'comma-dangle': [2, 'never'],
+    'comma-style': [2, 'last'],
+    'eqeqeq': 2,
+    'indent': [2, 2, { 'VariableDeclarator': 2 }],
     'no-eq-null': 2,
     'no-extra-parens': 2,
+    'no-extra-semi': 2,
     'no-lonely-if': 2,
+    'no-multi-spaces': 0,
     'no-nested-ternary': 2,
     'no-param-reassign': 2,
     'no-self-compare': 2,
+    'no-shadow': 2,
     'no-throw-literal': 2,
-    'no-void': 2,
     'no-undef': 2,
-    'no-extra-semi': 2,
     'no-underscore-dangle': 0,
-    'eqeqeq': 2,
-    'comma-style': [2, 'last'],
+    'no-void': 2,
+    'quotes': [2, 'single'],
     'semi': [2, 'always']
   }
-}
+};
